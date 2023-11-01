0b4d4cd9c935e48cc2e7c1bc5c0512d662432d01
jeffassis <cmdtjeff@gmail.com>
2023-11-01 10:04:51 -0300

    table disciplina

--- src/components/pages/Disciplina.js
+++ src/components/pages/Disciplina.js
@@ -1,22 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from '../form/Input'
 import SubmitButton from '../form/SubmitButton'
 import styles from './Disciplina.module.css'
+import api from '../../components/services/api'
 
 function Disciplina() {
+    // Recuperar token
+    const token = localStorage.getItem('token');
+
+    const [disciplinas, setDisciplinas] = useState([]);
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        api.get('disciplinas', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                page: 0,
+                limit: 5,
+                direction: 'asc'
+            }
+        }).then(response => {
+            setDisciplinas(response.data.content)
+        })
+    }, []);
+
+    async function cadastrar(e) {
+        e.preventDefault();
+        const data = {
+            nome,
+        }
+        try {
+            await api.post('/disciplinas', data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            alert('Cadastrado com sucesso!');
+        } catch (error) {
+            alert('Erro ao cadastrar!');
+        }
+    }
+
     return (
         <div>
             <div className={styles.Container}>
                 <h1>Cadastro de Disciplina</h1>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={cadastrar}>
                     <Input
                         type="text"
                         text="Disciplina"
-                        name="cadastrar"
+                        name="nome"
                         placeholder="Digite a disciplina..."
+                        handleOnChange={e => setNome(e.target.value)}
+                        value={nome}
                     />
                     <SubmitButton type='submit' text='Cadastrar' />
                 </form>
+            </div>
+            <div>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Disciplina</th>
+                            <th>Selecionar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            disciplinas.map((disciplina, indice) => (
+                                <tr key={indice}>
+                                    <td>{indice + 1}</td>
+                                    <td>{disciplina.nome}</td>
+                                    <td><button>Selecionar</button></td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     );
--- src/components/pages/Professor.js
+++ src/components/pages/Professor.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 
 import './Professor.module.css';
+import Input from '../form/Input'
+import SubmitButton from '../form/SubmitButton'
 
 import logoEscola from '../../assets/logoEscola.png'
 
@@ -20,12 +22,33 @@ export default function Professor() {
 
             <div className='content'>
                 <form>
-                    <input placeholder='Nome...' />
-                    <input placeholder='Rg...' />
+                    <Input
+                        type="text"
+                        text="Nome"
+                        name="nome"
+                        placeholder="Digite o nome..."
+                    />
+                    <Input
+                        type="text"
+                        text="RG"
+                        name="rg"
+                        placeholder="Digite o rg..."
+                    />
+                    <Input
+                        type="text"
+                        text="CPF"
+                        name="cpf"
+                        placeholder="Digite o cpf..."
+                    />                   
+                    <Input
+                        type="text"
+                        text="Telefone"
+                        name="telefone"
+                        placeholder="Digite o telefone..."
+                    />
                     <input type='date' />
-                    <input placeholder='Telefone...' />
 
-                    <button className='button' type='submit'>Add</button>
+                    <SubmitButton type='submit' text='Cadastrar' />
                 </form>
             </div>
         </div>
